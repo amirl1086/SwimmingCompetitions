7ec0cb9c866bf6cbb9364212a009750b42bc29e1
root <amirl1086@gmail.com>
2018-06-08 09:43:25 +0300

    added upload images to the cloud

--- android/app/src/main/java/com/app/swimmingcompetitions/swimmingcompetitions/ViewMediaActivity.java
+++ android/app/src/main/java/com/app/swimmingcompetitions/swimmingcompetitions/ViewMediaActivity.java
@@ -1,43 +1,37 @@
 package com.app.swimmingcompetitions.swimmingcompetitions;
 
-import android.Manifest;
-import android.Manifest.permission;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
+
 
 public class ViewMediaActivity extends LoadingDialog {
 
@@ -45,23 +39,14 @@ public class ViewMediaActivity extends LoadingDialog {
     private FirebaseUser fbUser;
     private FirebaseAuth mAuth;
     private JSON_AsyncTask jsonAsyncTaskPost;
-    String mCurrentPhotoPath;
+    private FirebaseStorage storage;
 
-    private Uri photoURI;
-
-    private Bitmap currentBitmap, calculatedBitmap;
-    private ImageButton analyze_btn;
-    private String currentPhotoPath, currentGalleryPath;
-    private static final int ACTION_IMAGE_CAPTURE = 1;
-    private static final int ACTION_GET_CONTENT = 2;
-    private static final int REQUEST_CAMERA = 100;
-    private String imageName = "";
     private Uri currentUri;
-
+    private ImageView mImageView;
+    private static final int ACTION_IMAGE_CAPTURE = 1;
 
     private DrawerLayout mDrawerLayout;
     private NavigationView navigationView;
-    private ImageView mImageView;
 
 
     @Override
@@ -75,6 +60,8 @@ public class ViewMediaActivity extends LoadingDialog {
             this.mAuth = FirebaseAuth.getInstance();
             this.fbUser = this.mAuth.getCurrentUser();
             this.mImageView = findViewById(R.id.image);
+
+            this.storage = FirebaseStorage.getInstance("gs://firebase-swimmingcompetitions.appspot.com");
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
@@ -95,6 +82,7 @@ public class ViewMediaActivity extends LoadingDialog {
             File photoFile = null;
             try {
                 photoFile = createImageFile();
+
             }
             catch (Exception e) {
                 showToast("שגיאה בהפעלת המצלמה, נסה לאתחל את האפליקציה");
@@ -104,29 +92,28 @@ public class ViewMediaActivity extends LoadingDialog {
 
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                photoURI = FileProvider.getUriForFile(this,
+                this.currentUri = FileProvider.getUriForFile(this,
                         "com.app.swimmingcompetitions.swimmingcompetitions.fileprovider", photoFile);
-                getBaseContext().grantUriPermission("com.app.swimmingcompetitions.swimmingcompetitions", photoURI, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                getBaseContext().grantUriPermission("com.app.swimmingcompetitions.swimmingcompetitions", this.currentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, this.currentUri);
                 startActivityForResult(takePictureIntent, ACTION_IMAGE_CAPTURE);
             }
         }
     }
 
-    private File createImageFile() throws IOException {
+    private File createImageFile() throws Exception {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        imageName = imageFileName;
+
         // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
+        return File.createTempFile(
+                imageFileName, // prefix
+                ".jpg", // suffix
+                storageDir // directory
+        );
     }
 
     @Override
@@ -135,21 +122,36 @@ public class ViewMediaActivity extends LoadingDialog {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK) {
-
-            //histogram_btn.setEnabled(true);
-
-
-            switch(requestCode) {
-                case ACTION_IMAGE_CAPTURE: //in case user is taking a picture
-
+            switch (requestCode) {
+                case ACTION_IMAGE_CAPTURE: {//in case user took a picture
+                    uploadImageToFirebaseStorage();
                     break;
-
-                case ACTION_GET_CONTENT: //in case user is loading picture from gallery
-
-                    break;
-
+                }
             }
         }
+    }
+
+    public void uploadImageToFirebaseStorage() {
+        StorageReference mainStorageRef = this.storage.getReference();
+        StorageReference riversRef = mainStorageRef.child("pics/" + this.currentUri.getLastPathSegment());
+        UploadTask uploadTask = riversRef.putFile(this.currentUri);
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                showToast("שגיאה בהעלאת התמונה לענן, נסה שוב");
+                System.out.println("ViewMediaActivity processFinish Exception \nMessage: " + e.getMessage() + "\nStack Trace: ");
+                e.printStackTrace();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                showToast("התמונה הועלתה בהצלחה");
+
+                //TODO add to gallery
+            }
+        });
     }
 
     public void showToast(String message) {
@@ -159,7 +161,7 @@ public class ViewMediaActivity extends LoadingDialog {
     @Override
     public void onStart() {
         super.onStart();
-        if(this.fbUser == null) {
+        if (this.fbUser == null) {
             switchToLogInActivity();
         }
     }
@@ -167,14 +169,14 @@ public class ViewMediaActivity extends LoadingDialog {
     @Override
     public void onResume() {
         super.onResume();
-        if(this.fbUser == null) {
+        if (this.fbUser == null) {
             switchToLogInActivity();
         }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case android.R.id.home: {
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
@@ -208,10 +210,9 @@ public class ViewMediaActivity extends LoadingDialog {
             actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
 
-        if(this.currentUser.getType().equals("parent") || this.currentUser.getType().equals("coach")) {
+        if (this.currentUser.getType().equals("parent") || this.currentUser.getType().equals("coach")) {
             this.navigationView.inflateMenu(R.menu.parent_home_side_bar_menu);
-        }
-        else if(this.currentUser.getType().equals("student")) {
+        } else if (this.currentUser.getType().equals("student")) {
             this.navigationView.inflateMenu(R.menu.student_home_side_bar_menu);
         }
 
